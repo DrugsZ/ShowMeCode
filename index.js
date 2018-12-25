593ec1d813e6ef6279dd0b4d85d8efc22c5d6610
DrugsZ <1002881508@qq.com>
2018-12-25 23:00:33 +0800

    fix: auto fix

--- index.js
+++ index.js
@@ -1,18 +1,11 @@
-import { createElm, VNode,normalizeChildren } from './VNode';
-import {patch} from './patch'
-function createElement( tag, props, ...children ) {
-  if(children){
-    children = normalizeChildren(children)
-  }
-  return new VNode(tag,props,children)
-}
-
-  let a = 7
+import {createElm,createElement} from './src/index.js';
+// const  = VDOM
+  let a = 7124
   let b = 4
   let c = true
   const element = (
     <div>
-        hello<span key={a} style={{width:"100px", height:'100px',backgroundColor:'red'}}>world! {c}</span>
+        hello123<span key={a} style={{width:"100px", height:'100px',backgroundColor:'red'}}>world! {c}</span>
     </div>
   );
 
@@ -23,6 +16,3 @@ function createElement( tag, props, ...children ) {
   let elm = createElm(element)
   document.body.appendChild(elm)
   let elm_1 = createElm(element_1)
-
-
-// patch(element,element_1)
