6fddce0ee2dab4733614b83f1d138f7dccfc4b77
DrugsZ <1002881508@qq.com>
2018-12-25 17:47:37 +0800

    feat:借鉴`vue``vdom`中的方法以实现对`string,number,sybmol,boolean`类型和`vnode`类型的同意处理

--- index.js
+++ index.js
@@ -1,23 +1,28 @@
-import { createElm, VNode } from './VNode';
+import { createElm, VNode,normalizeChildren } from './VNode';
 import {patch} from './patch'
-console.log(createElm)
 function createElement( tag, props, ...children ) {
+  if(children){
+    children = normalizeChildren(children)
+  }
   return new VNode(tag,props,children)
 }
-let a = 7
-let b = 4
-const element = (
-  <div>
-      hello<span key={a} style={{width:"100px", height:'100px',backgroundColor:'red'}}>world!</span>
-  </div>
-);
 
-const element_1 = (
-  <h3 key={b}>2922</h3>
-);
+  let a = 7
+  let b = 4
+  let c = true
+  const element = (
+    <div>
+        hello<span key={a} style={{width:"100px", height:'100px',backgroundColor:'red'}}>world! {c}</span>
+    </div>
+  );
 
-let elm = createElm(element)
-document.body.appendChild(elm)
-let elm_1 = createElm(element_1)
+  const element_1 = (
+    <h3 key={b}>2922</h3>
+  );
 
-patch(element,element_1)
+  let elm = createElm(element)
+  document.body.appendChild(elm)
+  let elm_1 = createElm(element_1)
+
+
+// patch(element,element_1)
