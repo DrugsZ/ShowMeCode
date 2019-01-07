96fc7a529daf8c8cf2d53edf120354b5652f00ce
DrugsZ <1002881508@qq.com>
2019-01-07 18:18:29 +0800

    test(*): 更新测试代码

--- index.js
+++ index.js
@@ -1,24 +1,32 @@
 import init, { createElement } from './src/index';
+import classModules from './src/modules/class';
 
-const patch = init();
+const patch = init([classModules]);
 let a = 7124;
+let b = 'ttt';
 let oldnode = null;
 
 const test = () => (
-  <div>
+  <div className={b}>
     <h3><span>{a}</span></h3>
   </div>
 );
 
-let element = test();
+const element = (
+  <div className="tttt ccc">
+    <h3><span>{a}</span></h3>
+  </div>
+);
+
 
 document.body.appendChild(patch(undefined, element).elm);
 oldnode = element;
 a += 1;
 
 setInterval(() => {
-  a += 1;
-  element = test();
-  patch(oldnode, element);
-  oldnode = element;
+  a++;
+  b = b === 'ttt' ? 'ccc' : 'ttt';
+  const elementOne = test();
+  patch(oldnode, elementOne);
+  oldnode = elementOne;
 }, 1000);
