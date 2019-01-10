39c278af30ed13153a1112460919822bae9f777b
DrugsZ <1002881508@qq.com>
2019-01-10 17:17:29 +0800

    test(test): 事件处理模块测试
    增加事件处理模块引入，

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import init, { createElement } from './src/index';
 import classModules from './src/modules/class';
 import styleModules from './src/modules/style';
+import eventModules from './src/modules/event';
 
-const patch = init([classModules, styleModules]);
+const patch = init([classModules, styleModules, eventModules]);
 let a = 7124;
 let b = 'ttt';
 let oldnode = null;
@@ -15,9 +16,26 @@ const styleOne = {
   opacity: 0,
 };
 
+const eventT = (str, event) => {
+  console.log(str, event);
+};
+
+const eventTest = eventT.bind(this, 123);
+
+const eventTestTwo = eventT.bind(this, 222);
+
+let evT = eventTest;
+
+const eventTest_1 = () => console.log(111);
 let testStyle = styleOne;
 const test = () => (
-  <div className={b} style={testStyle}>
+  <div
+    className={b}
+    style={testStyle}
+    onClick={
+      evT
+    }
+  >
     <h3><span>{a}</span></h3>
   </div>
 );
@@ -36,6 +54,7 @@ a += 1;
 setInterval(() => {
   a++;
   b = b === 'ttt' ? 'ccc' : 'ttt';
+  evT = evT === eventTest ? eventTestTwo : eventTest;
   testStyle = testStyle === styleOne ? style : styleOne;
   const elementOne = test();
   patch(oldnode, elementOne);
