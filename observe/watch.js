a9ac14cc41bd3296b268da6ac7a09b4566e4f86d
DrugsZ <1002881508@qq.com>
2019-01-23 23:27:00 +0800

    feat(core): 调整取值方式

--- observe/watch.js
+++ observe/watch.js
@@ -1,3 +1,5 @@
+// import { parsePath } from '../src/util';
+
 const setTarget = (target) => {
   target.target = null;
 };
@@ -8,13 +10,15 @@ class Watcher {
     this.cb = cb;
     this.expOrFn = expOrFn;
     this.content = content;
+    this.getter = parsePath(this.expOrFn);
 
     this.get();
   }
 
   get() {
     pushTarget(this.cb);
-    const value = this.content[this.expOrFn];
+    // const value = this.content[this.expOrFn];
+    const value = this.getter.call(this.content, this.content);
     Watcher.target = null;
     return value;
   }
